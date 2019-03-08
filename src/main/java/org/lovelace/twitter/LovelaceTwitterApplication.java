0c5a6fe745290db26a6765d72a9f5111bdb0dadf
Jimmy Heller <ali.tommy.heller2009@gmail.com>
2019-03-09 00:15:57 +0330

    Initial Commit of the project to github repository

---
+++ src/main/java/org/lovelace/twitter/LovelaceTwitterApplication.java
@@ -0,0 +1,13 @@
+package org.lovelace.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovelaceTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovelaceTwitterApplication.class, args);
+	}
+
+}
