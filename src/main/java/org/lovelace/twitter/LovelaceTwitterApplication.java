a3f1500f9caae9db225a9e0309278eebca1ab5b6
rahmaniz <z.rahmani1217@gmail.com>
2019-03-15 19:51:28 +0330

    'FirstVerify'

--- src/main/java/org/lovelace/twitter/LovelaceTwitterApplication.java
+++ src/main/java/org/lovelace/twitter/LovelaceTwitterApplication.java
@@ -2,8 +2,11 @@ package org.lovelace.twitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 public class LovelaceTwitterApplication {
 
 	public static void main(String[] args) {
