1167e078d25c6957782b01cf655fb6c73eac8459
Jimmy Heller <ali.tommy.heller2009@gmail.com>
2019-03-23 01:37:04 +0430

    adding keys to the controller

--- src/main/java/org/lovelace/twitter/web/controller/BasicInfoController.java
+++ src/main/java/org/lovelace/twitter/web/controller/BasicInfoController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/basicinfo")
 public class BasicInfoController {
 
+    private static final String CONSUMER_KEY = "";
+    private static final String CONSUMER_SECRET = "";
+    private static final String ACCESS_TOKEN = "";
+    private static final String ACCESS_TOKEN_SECRET = "";
 
     @GetMapping
     public String getBasicInfo() {
